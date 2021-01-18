ada40127da03a880291252f51b10c97d39494f95
Sylvester <sylvestre.ofueu@exquisappfactory.com>
2021-01-18 14:47:11 +0100

    My first commit

---
+++ src/main/java/com/example/multipurpose/MultipurposeApplication.java
@@ -0,0 +1,13 @@
+package com.example.multipurpose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipurposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipurposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multipurpose/MultipurposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multipurpose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipurposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
